6094dd73713c3129af92f28d5b982c2aaa7b5901
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-05-26 18:54:09 +0000

    Deploy ismailarilik.com to github.com/ismailarilik/ismailarilik.github.io.git:gh-pages

---
+++ precache-manifest.e7e2d8a3d75eac5966938fbaa2109d35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d052a7acbcc6d09ac9f90034340aed8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb2431fb3de21e2d986e",
+    "url": "/js/app.2cf5a453.js"
+  },
+  {
+    "revision": "7eb76b3a15e68bb817fa",
+    "url": "/js/chunk-vendors.c925ce74.js"
+  },
+  {
+    "revision": "19e8aa2e598522eb93fa1ca2aa82f536",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
