59a84cb4db3e87f50015768c192f353533995109
ismailarilik <arilik.ismail@gmail.com>
2022-08-29 11:09:32 +0300

    Remove unnecessary code
    I intend to use just HTML, CSS and JS for my website

--- cypress.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env node */
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
-  e2e: {
-    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
-    baseUrl: "http://localhost:4173",
-  },
-});
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env node */
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
